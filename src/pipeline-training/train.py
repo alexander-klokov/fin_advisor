00146b0b7f17f27e989a39da8e60cf7d6d4ae711
Alexander Klokov <alexander.klokov@gmail.com>
2024-02-08 16:59:37 -0600

    start training by tutorial

--- src/pipeline-training/train.py
+++ src/pipeline-training/train.py
@@ -1,8 +1,17 @@
 import fire
-
 import logging
 
+import numpy as np
+import evaluate
+
+from datasets import load_dataset
+
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from transformers import TrainingArguments
+
 from ..utils.log_memory import log_gpu_memory, log_ram
+
 
 # def train(data: str, output: str):
 def train():
@@ -16,6 +25,41 @@ def train():
     log_ram()
 
     logger.info("#" * 64)
+
+    dataset = load_dataset("yelp_review_full")
+
+    print(dataset["train"][100])
+
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+    def tokenize_function(examples):
+        return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+    tokenized_datasets = dataset.map(tokenize_function, batched=True)
+
+    small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
+    small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
+
+    model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=5)
+
+    training_args = TrainingArguments(output_dir="test_trainer")
+
+    metric = evaluate.load("accuracy")
+
+    def compute_metrics(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=predictions, references=labels)
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=small_train_dataset,
+        eval_dataset=small_eval_dataset,
+        compute_metrics=compute_metrics,
+    )
+    
+    trainer.train()
 
 
 if __name__ == "__main__":
